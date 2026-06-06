d295ff6a6ed7aa6c08fd7f756bc06437c5bf0492
Mehran <mehran613.niakan@gmail.com>
2026-06-06 15:55:11 +0330

    modify Matrix.py

--- Ch1/Projects/Matrix.py
+++ Ch1/Projects/Matrix.py
@@ -1,11 +1,11 @@
 
 def get_user_input(count):
     if count == 1:
-        user_input = list(map(int, input(f'Enter {count}st row of matrix :').split()))
+        user_input = list(map(int, input(f'Enter {count}st row of matrix (space between) :').split()))
     elif count == 2:
-        user_input = list(map(int, input(f'Enter {count}nd row of matrix :').split()))
+        user_input = list(map(int, input(f'Enter {count}nd row of matrix (space between) :').split()))
     else:
-        user_input = list(map(int, input(f'Enter {count}th row of matrix :').split()))
+        user_input = list(map(int, input(f'Enter {count}th row of matrix (space between) :').split()))
 
     return user_input
 
@@ -45,6 +45,8 @@ def calculator():
 
     matrix = matrix_setter()
 
+    print(50 * '=')
+
     counter = 0
 
     for i in matrix:
@@ -62,5 +64,10 @@ def calculator():
         else:
             print(f'Sum of {counter}th row of matrix is: {row_sum}')
 
+    print(50*'=')
+    column_sum = [sum(col) for col in zip(*matrix)]
+
+    for i, column_sum in enumerate(column_sum, 1):
+        print(f"Sum of {i}th column of matrix is: {column_sum}")
 
 calculator()
